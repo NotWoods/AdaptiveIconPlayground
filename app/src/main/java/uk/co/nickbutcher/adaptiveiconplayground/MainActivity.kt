a51a063f5ff63f699e60edebaed581d8dbe5374d
Tiger Oakes <toakes@mozilla.com>
2019-11-15 10:48:16 -0800

    Migrate to AndroidX

--- app/src/main/java/uk/co/nickbutcher/adaptiveiconplayground/MainActivity.kt
+++ app/src/main/java/uk/co/nickbutcher/adaptiveiconplayground/MainActivity.kt
@@ -20,18 +20,6 @@ import android.content.Intent
 import android.graphics.Rect
 import android.graphics.drawable.AdaptiveIconDrawable
 import android.os.Bundle
-import android.support.animation.FloatPropertyCompat
-import android.support.animation.SpringAnimation
-import android.support.annotation.ColorInt
-import android.support.annotation.DrawableRes
-import android.support.annotation.FloatRange
-import android.support.design.widget.BottomSheetBehavior
-import android.support.v4.app.LoaderManager
-import android.support.v4.content.AsyncTaskLoader
-import android.support.v4.content.Loader
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.transition.ChangeBounds
 import android.transition.Fade
 import android.transition.TransitionManager
@@ -45,9 +33,21 @@ import android.view.View.GONE
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
-import android.widget.LinearLayout.HORIZONTAL
-import android.widget.LinearLayout.VERTICAL
 import android.widget.SeekBar
+import androidx.annotation.ColorInt
+import androidx.annotation.DrawableRes
+import androidx.annotation.FloatRange
+import androidx.appcompat.app.AppCompatActivity
+import androidx.dynamicanimation.animation.FloatPropertyCompat
+import androidx.dynamicanimation.animation.SpringAnimation
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.AsyncTaskLoader
+import androidx.loader.content.Loader
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.HORIZONTAL
+import androidx.recyclerview.widget.RecyclerView.VERTICAL
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainActivity : AppCompatActivity() {
 
